475d9cb30c46f934e404ed00bae38352d6ab5ae3
WaiPhyoMaung <waiphyomaung7@gmail.com>
2023-06-01 22:54:55 +0630

    Banner Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Banner from '@/components/Banner'
 import { Header } from '@/components/Header'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="mx-auto ">
+        {/* banner */}
+        <Banner />
+
+        {/* product feed */}
+      </main>
     </>
   )
 }
---
+++ components/Banner.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
+import { Carousel } from 'react-responsive-carousel'
+
+const Banner = () => {
+  return (
+    <div className="relative bg-amazon_blue-light">
+      <div className="absolute w-full h-64 bg-gradient-to-t from-gray-950 to-transparent bottom-0 z-50" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showIndicators={false}
+        showThumbs={false}
+        showStatus={false}
+        interval={5000}
+        // width={10000}
+      >
+        <div className="">
+          <img
+            className="rounded-t-3xl object-contain"
+            loading="lazy"
+            src="https://m.media-amazon.com/images/I/61qa3132IFL._SX3000_.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="https://m.media-amazon.com/images/I/71tIrZqybrL._SX3000_.jpg"
+            alt=""
+            className="rounded-t-3xl"
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="https://m.media-amazon.com/images/I/61q5Pg3hO8L._SX3000_.jpg"
+            alt=""
+            className="rounded-t-3xl"
+          />
+        </div>
+      </Carousel>
+    </div>
+  )
+}
+
+export default Banner
